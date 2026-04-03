7ca228eee61b970c28d57a05c23987db6b08045f
OsaBinawa <OsaBinawa@users.noreply.github.com>
2026-04-03 21:02:03 +0700

    initialize

---
+++ Assets/Script/Buff/WeaponSelect.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponSelect : MonoBehaviour
+{
+    [Header("Data")]
+    public WeaponSO[] choices; // your ScriptableObjects
+
+    [Header("UI")]
+    public GameObject choicePrefab;   // prefab (Button + Text + Image)
+    public Transform parentContainer; // parent GameObject
+
+    private WeaponSO selectedChoice;
+    [SerializeField] private Inventory playerInventory;
+
+    private void Awake()
+    {
+        playerInventory = FindFirstObjectByType<Inventory>();
+        
+    }
+
+    void Start()
+    {
+        GenerateChoices();
+    }
+
+    void GenerateChoices()
+    {
+        Debug.Log("Total choices: " + choices.Length);
+        foreach (var choice in choices)
+        {
+            Debug.Log("Spawning: " + choice.WeaponName);
+            GameObject obj = Instantiate(choicePrefab, parentContainer);
+            
+            // Get UI components directly
+            Text text = obj.GetComponentInChildren<Text>();
+            Image image = obj.GetComponentInChildren<Image>();
+            Button button = obj.GetComponentInChildren<Button>();
+
+            // Set data
+            if (text != null)
+                text.text = choice.WeaponName;
+
+            if (image != null && choice.icon != null)
+                image = choice.icon;
+
+            // Button click
+            button.onClick.AddListener(() => SelectChoice(choice));
+        }
+    }
+
+    void SelectChoice(WeaponSO choice)
+    {
+        playerInventory.AddWeapon(choice);
+        Debug.Log("Selected: " + choice.WeaponName);
+    }
+}
--- Assets/Script/WeaponSO.cs
+++ Assets/Script/WeaponSO.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(menuName = "Weapon")]
 public class WeaponSO : ScriptableObject
@@ -6,6 +7,7 @@ public class WeaponSO : ScriptableObject
     public string WeaponName;
     public int AttackBonus;
     public int skillCost;
+    public Image icon;
     public ElementType Element;
     public AnimationClip AttackAnimation;
     public AttackEffect[] Effects;
