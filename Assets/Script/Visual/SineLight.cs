1ae98f7ae5f133d1711e57a729c15a7da4e1af3c
givenariel <140182956+givenariel@users.noreply.github.com>
2026-05-01 11:04:30 +0700

    Sine wave light + Weapon Selection UI

---
+++ Assets/Script/Visual/SineLight.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class SineLight : MonoBehaviour
+{
+    public float baseIntensity = 1f;  
+    public float amplitude = 1f;
+    public float frequency = 1f;       
+
+    private Light2D light2D;
+
+    void Start()
+    {
+        light2D = GetComponent<Light2D>();
+    }
+
+    void Update()
+    {
+        float sineValue = Mathf.Sin(Time.time * frequency);
+        float remapped = (sineValue + 1f) * 0.5f;
+        light2D.intensity = baseIntensity + (amplitude * remapped);
+    }
+}
