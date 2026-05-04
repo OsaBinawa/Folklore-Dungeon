1abeedb0fc761ef4d930233fe5771634403face8
OsaBinawa <OsaBinawa@users.noreply.github.com>
2026-05-04 19:23:20 +0700

    EliteMiss(Not tested) + Kurang Elite Broken

---
+++ Assets/Prefabs/Enemy/Elite/EliteMiss/EliteMissEnemy.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class EliteMissEnemy : EnemyUnit
+{
+    [Header("Miss Settings")]
+    [SerializeField] private float delayChance = 0.4f;
+    [SerializeField] private float delayAmount = 25f;
+    public override void Act(PlayerUnit player)
+    {
+        base.Act(player);
+    }
+    public void TryApplyDelay()
+    {
+        PlayerUnit player = FindFirstObjectByType<PlayerUnit>();
+
+        if (player == null) return;
+
+        if (Random.value <= delayChance)
+        {
+            Debug.Log($"{name} applies delay!");
+
+            turnManager?.ModifyAV(player, delayAmount);
+        }
+    }
+
+}
