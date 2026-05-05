7bc343a4dfd29a3a4f5466a4ae369fdf285c2f1a
OsaBinawa <OsaBinawa@users.noreply.github.com>
2026-05-05 14:33:01 +0700

    secara teori udah bener, belom di tes

---
+++ Assets/Prefabs/Enemy/Elite/EliteBroken/EliteBroken.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class EliteBroken : BasicBrokenEnemy
+{
+    private int currentShield;
+    [SerializeField] private GameObject Vfx_Shield;//VFX atau apapun lah
+    [SerializeField] private float shieldChance = 0.5f;
+    public override void Act(PlayerUnit player)
+    {
+        Debug.Log(name + " TypoEnemy Act");
+        if (Random.value <= shieldChance && currentShield <= 0)
+        {
+            Debug.Log("Using Shield");
+            UseShield();
+            OnActionFinished();
+            return;
+        }
+        Debug.Log("Using normal attack");
+        base.Act(player);
+    }
+
+    private void UseShield()
+    {
+        currentShield = Mathf.RoundToInt(data.MaxHP * 0.5f);
+
+        Debug.Log($"{name} used Shield: {currentShield}");
+
+
+        anim.SetTrigger("Shield");
+        if (Vfx_Shield != null) { Vfx_Shield.SetActive(true); }
+
+    }
+
+    public override void TakeDamage(int damage, ElementType element)
+    {
+        if (currentShield > 0)
+        {
+            int absorbed = Mathf.Min(currentShield, damage);
+            currentShield -= absorbed;
+            damage -= absorbed;
+
+            Debug.Log($"Shield absorbed {absorbed}, remaining shield: {currentShield}");
+        }
+
+        if (damage > 0)
+            base.TakeDamage(damage, element);
+    }
+
+    public void DisableShield()
+    {
+        Vfx_Shield.SetActive(false);
+    }
+}
