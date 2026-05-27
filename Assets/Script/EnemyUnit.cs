614495eb7acbd83a730bd83115a21a9ccb7aa2b2
givenariel <140182956+givenariel@users.noreply.github.com>
2026-05-27 22:05:29 +0700

    Announce easy and medium enemy

--- Assets/Script/EnemyUnit.cs
+++ Assets/Script/EnemyUnit.cs
@@ -11,6 +11,7 @@ public class EnemyUnit : MonoBehaviour, IPointerClickHandler
     [Header("Data")]
     [SerializeField] protected EnemyData data;
     public static event Action OnEnemyDied;
+    public static event Action<string> OnEnemyAct;
     
     [Header("Runtime")]
     [SerializeField] protected int currentHP;
@@ -107,6 +108,7 @@ public class EnemyUnit : MonoBehaviour, IPointerClickHandler
         ExecuteAttack(attacks[atkIndex], player);
         OnActionFinished();
         Debug.Log("test");
+        OnEnemyAct?.Invoke(data.name.Replace("(Clone)", "") + " performed " + attacks[atkIndex].Name);
     }
     public virtual void Act(PlayerUnit player)
     {
