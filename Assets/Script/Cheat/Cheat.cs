d15e91427568a55a289a39f3fec1c80690409b79
OsaBinawa <OsaBinawa@users.noreply.github.com>
2026-05-29 08:13:40 +0700

    something

--- Assets/Script/Cheat/Cheat.cs
+++ Assets/Script/Cheat/Cheat.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class Cheat : MonoBehaviour
 {
+    public GameObject debugButton;
+
+    private void Update()
+    {
+        if (Keyboard.current.leftCtrlKey.isPressed &&
+            Keyboard.current.hKey.wasPressedThisFrame)
+        {
+            debugButton.SetActive(true);
+            Debug.Log("Cheat activated");
+        }
+    }
     public void RestartScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+
 }
+    
