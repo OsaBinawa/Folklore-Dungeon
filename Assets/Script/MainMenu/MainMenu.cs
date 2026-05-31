a180a1ba5edb57495ca6a5dee52c41c1f337630a
OsaBinawa <OsaBinawa@users.noreply.github.com>
2026-05-31 09:16:33 +0700

    Volume Mixer

--- Assets/Script/MainMenu/MainMenu.cs
+++ Assets/Script/MainMenu/MainMenu.cs
@@ -1,11 +1,35 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private AudioClip mainMenuBGM;
     public List<GameObject> panels = new();
+    public AudioMixer mixer;
+    [SerializeField] private Slider BGM_Slider;
+    [SerializeField] private Slider SFX_Slider;
+    public void UpdateMusicVolume()
+    {
+        float value = BGM_Slider.value;
+
+        // Avoid Log10(0)
+        value = Mathf.Max(value, 0.0001f);
+
+        mixer.SetFloat("BGMVolume", Mathf.Log10(value) * 20f);
+    }
+    public void UpdateSFXVolume()
+    {
+        float value = SFX_Slider.value;
+
+        // Avoid Log10(0)
+        value = Mathf.Max(value, 0.0001f);
+
+        mixer.SetFloat("SFXVolume", Mathf.Log10(value) * 20f);
+    }
+
     public void ShowPanel(GameObject panelToShow)
     {
         if (panels != null)
